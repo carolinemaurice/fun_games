9b374030cb397cc027163763e156d2adb1036ad1
Laurie Segalowitch <lausegalo@gmail.com>
2023-02-28 10:41:27 +0100

    game show ini -waiting for seeds still

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,10 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+puts "COUCOU"
 Game.create(name: "Catan", city: "Toulouse", price_per_day: 5, minimum_age: 8, players: 4)
 Game.create(name: "Uno", city: "Bordeaux", price_per_day: 3, minimum_age: 5, players: 8)
 Game.create(name: "Perudo", city: "Toulouse", price_per_day: 4, minimum_age: 6, players: 6)
 Game.create(name: "Monopoly", city: "Paris", price_per_day: 8, minimum_age: 7, players: 4)
+
+puts "FINI"
