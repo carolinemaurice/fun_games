edaa8c0d8f632f92ba790f46360ae2a4e9e9e4c5
eliot rafat offor <eliot.rafat.offor@gmail.com>
2023-03-01 16:27:50 +0100

    add geocoder in game show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_28_130656) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_142441) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,11 +59,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_130656) do
     t.integer "players"
     t.integer "minimum_age"
     t.string "description"
-    t.string "city"
+    t.string "address"
     t.float "price_per_day"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_games_on_user_id"
   end
 
