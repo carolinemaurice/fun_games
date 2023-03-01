0033be31fd6f73e510f34a868e39fe8f21eacfc2
Caroline MAURICE <caroline.maurice.henry@gmail.com>
2023-03-01 16:06:34 +0100

    dashboard page creation

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
+  skip_after_action :verify_authorized, only: [:dashboard]
   def home
+  end
+
+  def dashboard
+    @games = current_user.games
+    @bookings = current_user.bookings
   end
 end
