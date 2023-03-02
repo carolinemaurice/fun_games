f66e0fbe5fab8cbc1f30fc4d892d8842c24cf5cb
Caroline MAURICE <caroline.maurice.henry@gmail.com>
2023-03-02 17:31:08 +0100

    Add mapbox to index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   skip_after_action :verify_authorized, only: [:dashboard]
   def home
-    @games = Game.geocoded.last(5)
+    @games = Game.all.geocoded.last(5)
     @markers = @games.map do |game|
       {
         lat: game.latitude,
