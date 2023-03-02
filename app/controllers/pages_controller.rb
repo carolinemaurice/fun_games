c6419b913aac348d11872ccb40633eb664f123fe
Laurie Segalowitch <lausegalo@gmail.com>
2023-03-02 12:18:03 +0100

    mapbox ok sans window

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   skip_after_action :verify_authorized, only: [:dashboard]
   def home
+    @games = Game.geocoded.last(5)
+    @markers = @games.map do |game|
+      {
+        lat: game.latitude,
+        lng: game.longitude
+      }
+    end
   end
 
   def dashboard
